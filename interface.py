e033a2dc3b852f5b1755781f3f46fb02e4d79ffe
manelbenaissa <manel.benaissa@gmail.com>
2017-12-11 00:36:29 +0100

    add interface

--- interface.py
+++ interface.py
@@ -1,1 +1,14 @@
 """Interface between game and user."""
+
+from lib.Interactions import Interactions
+
+interact = Interactions()
+interact.welcoming()
+
+# The user has to choose a pool
+print('You have to choose a pool among the following files:')
+pool_list = interact.get_pools()
+for i in range(len(pool_list)):
+    print (i, ':', pool_list[i])
+
+poolfile = interact.get_pool_input()
