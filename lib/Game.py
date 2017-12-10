fd2270e9d089703ad9c067231f2c74cc0abc6b0e
manelbenaissa <manel.benaissa@gmail.com>
2017-12-10 21:38:54 +0100

    fix errors

--- lib/Game.py
+++ lib/Game.py
@@ -1,9 +1,10 @@
 """This module defines the 'Game' class, used to create a game between two,
 players."""
 
-from League import League
+from lib.League import League
 
-class Game(object):
+
+class Game:
     """class that simulate and apply game results"""
 
     def __init__(self, players, player1, player2):
@@ -12,17 +13,21 @@ class Game(object):
         self.player1 = player1
         self.player2 = player2
 
-
     def simulate(self):
-        """Give the expected result of the game based
-        on the elo of each player."""
-
+        """
+        Give the expected result of the game base on the elo of each player.
+        """
         winProbability = self.players.winProbability(self.player1, self.player2)
-        event = "wins" if winProbability >= 50 else "loses"
-        probability = winProbability if winProbability >= 50 else 100 - winProbability
+        print(winProbability)
+        if winProbability >= 0.5:
+            event = "wins"
+            probability = winProbability
+        else:
+            event = "loses"
+            probability = 1 - winProbability
         print("Player1 {} with {} of probability".format(event, probability))
 
-    def result(self,result):
+    def result(self, result):
         """apply the elo modification depeding on the result of the game"""
 
         self.players.modify(self.player1, self.player2, result)
