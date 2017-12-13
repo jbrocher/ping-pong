90c68a2f21ae37b693bb41a974ad5da53364433b
jb <jeanb.rocher@gmail.com>
2017-12-13 16:36:35 +0100

    add a funtion to reset the test file, and call it at the beginning of each test in League

--- test.py
+++ test.py
@@ -29,7 +29,12 @@ class EloTest(unittest.TestCase):
 class LeagueTest(unittest.TestCase):
     """docstring for EloTest."""
 
+    def resetTestFile(self):
+        with open("data/unit-testing/players/players.json",'w') as testFile:
+            testFile.write('[{"name": "JB", "elo": 500}, {"name": "Manel", "elo": 300}, {"name": "Guilhem", "elo": 3000}]')
+
     def testWinProbability(self):
+        self.resetTestFile()
         league = League("data/unit-testing/players/players.json")
 
         prob = league.winProbability(0, 1)
@@ -37,6 +42,7 @@ class LeagueTest(unittest.TestCase):
             0.76, prob, 'testComppute fails, actula prob : {}'.format(prob))
 
     def testSearchPlayerByname(self):
+        self.resetTestFile()
         league = League("data/unit-testing/players/players.json")
         results = league.searchPlayerByName("JB")
         self.assertEqual(results, [(0, {
@@ -45,6 +51,7 @@ class LeagueTest(unittest.TestCase):
         })])
 
     def testAddPlayer(self):
+        self.resetTestFile()
         league = League("data/unit-testing/players/players.json")
         league.addPlayer({"name": "Biausser", "elo": 0})
 
@@ -53,34 +60,21 @@ class LeagueTest(unittest.TestCase):
             "name": "Biausser",
             "elo": 0
         })])
-        with open("data/unit-testing/players/players.json",'w') as testFile:
-            testFile.write('[{"name": "JB", "elo": 500}, {"name": "Manel", "elo": 300}, {"name": "Guilhem", "elo": 3000}]')
+
         with self.assertRaises(ValueError):
             league.addPlayer({"name": "JB", "elo": 4})
 
     def testDelPlayer(self):
+        self.resetTestFile()
         league = League("data/unit-testing/players/players.json")
         league.delPlayer("JB")
-
-
-        self.assertEqual(league.searchPlayerByName("JB"),[])
-
-        with open("data/unit-testing/players/players.json",'w') as testFile:
-            testFile.write('[{"name": "JB", "elo": 500}, {"name": "Manel", "elo": 300}, {"name": "Guilhem", "elo": 3000}]')
+        self.assertEqual(league.searchPlayerByName("JB"), [])
 
     def testModifyPlayer(self):
+        self.resetTestFile()
         league = League("data/unit-testing/players/players.json")
         league.modifyPlayer("JB", elo=600)
-
-
-        self.assertEqual(league.searchPlayerByName("JB"),[(0,{"name": "JB", "elo": 600})])
-
-        with open("data/unit-testing/players/players.json",'w') as testFile:
-            testFile.write('[{"name": "JB", "elo": 500}, {"name": "Manel", "elo": 300}, {"name": "Guilhem", "elo": 3000}]')
-
-
-
-
+        self.assertEqual(league.searchPlayerByName("JB"), [(0, {"name": "JB", "elo": 600})])
 
 
 unittest.main()
