658f177e871a5f877d1499b1655ab7a4e3375df0
manelbenaissa <manel.benaissa@selectra.info>
2017-12-15 15:09:05 +0100

    Add the menu to the interface

--- interface.py
+++ interface.py
@@ -1,8 +1,12 @@
 """Interface between game and user."""
 
 from lib.Interactions import Interactions
+from lib.Interactions import MainMenu
 
 interact = Interactions()
+menu = MainMenu()
+
+
 interact.welcoming()
 
 # The user has to choose a pool
@@ -12,3 +16,10 @@ for i in range(len(pool_list)):
     print (i, ':', pool_list[i])
 
 poolfile = interact.get_pool_input()
+print("Vale, we're going to work with pool:", poolfile)
+
+while True:
+    print("\nWhat do you want to do next?")
+    menu.possible_menu_inputs()
+    menu_choice = menu.get_menu_input()
+    menu.exec_menu(menu_choice, poolfile)
