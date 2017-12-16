29607b13a83b0fc53a22bcae892c9427476e4c4b
jb <jeanb.rocher@gmail.com>
2017-12-16 14:52:14 +0100

    Fix the order in which the menu items are displayed, that was previsously random

--- lib/Interactions.py
+++ lib/Interactions.py
@@ -60,8 +60,13 @@ class MainMenu:
 
     def possible_menu_inputs(self):
         """Display menu options."""
-        for i in self.possibilites:
-            print(i, ':', self.possibilites[i])
+        possibilitesList = []
+        for key, value in self.possibilites.items():
+            possibilitesList.append((key, value))
+        sortedPossibilitiesList = sorted(possibilitesList, key=lambda possibility: possibility[0])
+
+        for possibility in sortedPossibilitiesList:
+            print("{} : {}".format(possibility[0], possibility[1]))
 
     def get_menu_input(self):
         """Ask user to choose an action."""
